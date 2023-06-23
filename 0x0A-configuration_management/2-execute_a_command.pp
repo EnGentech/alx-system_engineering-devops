ef8d10477daed49432e888d39ae1cd0b10520a5a
EnGentech <engen.inyang@gmail.com>
2023-06-23 08:42:23 -0700

    modified

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,8 +1,12 @@
 #Its time to die, killme
 
-exec { 'killmenow_process':
-  command     => 'pkill killmenow',
-  path        => ['/usr/bin', '/usr/sbin', '/bin', '/sbin'],
-  onlyif      => 'pgrep killmenow',
-  refreshonly => true,
+#exec { 'killmenow_process':
+#  command     => 'pkill killmenow',
+#  path        => ['/usr/bin', '/usr/sbin', '/bin', '/sbin'],
+#  onlyif      => 'pgrep killmenow',
+#  refreshonly => true,
+#}
+
+exec{'pkill -f killmenow':
+path  => '/usr/bin/:/usr/local/bin/:/bin/'
 }
