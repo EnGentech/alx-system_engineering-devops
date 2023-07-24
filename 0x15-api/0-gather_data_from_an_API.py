61e10d3a65b20b9ec372567ec4f81fa62896153b
EnGentech <engen.inyang@gmail.com>
2023-07-24 13:07:49 -0700

    i am tired 

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,31 +3,32 @@
 import requests
 from sys import argv
 
-id = int(argv[1])
+if __name__ == "__main__":
+    id = int(argv[1])
 
-url = "https://jsonplaceholder.typicode.com/todos"
-url2 = "https://jsonplaceholder.typicode.com/users"
+    url = "https://jsonplaceholder.typicode.com/todos"
+    url2 = "https://jsonplaceholder.typicode.com/users"
 
-connect = requests.get(url).json()
-user = requests.get(url2).json()
+    connect = requests.get(url).json()
+    user = requests.get(url2).json()
 
-lst = []
-count = 0
-for done in connect:
-    if done['userId'] == id and done['completed'] is True:
-        lst.append(done['title'])
-        count += 1
-task = 0
-for x in connect:
-    if x['userId'] == id:
-        task += 1
+    lst = []
+    count = 0
+    for done in connect:
+        if done['userId'] == id and done['completed'] is True:
+            lst.append(done['title'])
+            count += 1
+    task = 0
+    for x in connect:
+        if x['userId'] == id:
+            task += 1
 
-for name in user:
-    if name['id'] == id:
-        print("Employee {} is done with tasks ({}/{})"
-              .format(name['name'], count, task))
+    for name in user:
+        if name['id'] == id:
+            print("Employee {} is done with tasks ({}/{})"
+                  .format(name['name'], count, task))
 
-for x in lst:
-    print("\t{}".format(x))
+    for x in lst:
+        print("\t{}".format(x))
 
 # Coded by EnGentech
